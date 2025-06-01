42e26411952d85774a2af326a40ee2d24b9c224b
Archerymaister <Marshmallow73.ls@gmail.com>
2025-06-01 23:57:33 +0200

    Add website toggle

--- content.js
+++ content.js
@@ -3,27 +3,32 @@ let targetImage = images[Math.floor(Math.random() * images.length)];
 let targetImageSource = targetImage.src;
 let alreadyFound = false;
 
-targetImage.addEventListener('mouseenter', function() {
-    targetImage.src = chrome.runtime.getURL('images/kevin.png');
-    if(!alreadyFound){
-        alreadyFound = true;
+chrome.storage.sync.get('urlList', function(result) {
+    let urlList = result.urlList || [];
 
-        chrome.storage.sync.get('counter', function(result) {
-            var value = result.counter;
+    if(!urlList.includes(window.location.origin)) {
+        targetImage.addEventListener('mouseenter', function() {
+                targetImage.src = chrome.runtime.getURL('images/kevin.png');
+                if(!alreadyFound){
+                    alreadyFound = true;
 
-            if(value == undefined){
-                value = 0;
-            } else {
-                value += 1;
-            }
+                    chrome.storage.sync.get('counter', function(result) {
+                        var value = result.counter;
 
-            chrome.storage.sync.set({ 'counter': value }, function() {
-                console.log('Data updated. Found Kevin ' + value + ' times!' );
+                        if(value == undefined){
+                            value = 0;
+                        } else {
+                            value += 1;
+                        }
+
+                        chrome.storage.sync.set({ 'counter': value });
+                    });
+                }
             });
-        });
+
+            targetImage.addEventListener('mouseleave', function() {
+                targetImage.src = targetImageSource
+            });
     }
 });
 
-targetImage.addEventListener('mouseleave', function() {
-    targetImage.src = targetImageSource
-});
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,36 @@
-
 chrome.storage.sync.get('counter', function(result) {
     document.getElementById("counterPlaceholder").innerHTML = result.counter;
+});
+
+document.getElementById("toggleThisPage").addEventListener("click", function(event) {
+    const isChecked = event.target.checked;
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        if (tabs.length > 0) {
+            const currentTabUrl = new URL(tabs[0].url).origin;
+            chrome.storage.sync.get('urlList', function(result) {
+                let urlList = result.urlList || [];
+
+                if (isChecked) {
+                    // Entferne die URL aus der Liste
+                    urlList = urlList.filter(url => url !== currentTabUrl);
+                } else {
+                    // Füge die URL zur Liste hinzu, falls sie noch nicht existiert
+                    if (!urlList.includes(currentTabUrl)) {
+                        urlList.push(currentTabUrl);
+                    }
+                }
+
+                chrome.storage.sync.set({ 'urlList': urlList });
+            });
+        } else {
+            console.log("No active tab found.");
+        }
+    });
+});
+
+chrome.storage.sync.get('urlList', function(result) {
+    let urlList = result.urlList || [];
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        document.getElementById("toggleThisPage").checked = !urlList.includes(new URL(tabs[0].url).origin);
+    });
 });
